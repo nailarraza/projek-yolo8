5bd6ef3d3eb1a9843eed05ec44a664f3b20f36b7
nailarraza <nailarraza16@gmail.com>
2025-06-01 20:30:14 +0700

    Tahap 10: Memastikan pengelolaan SECRET_KEY dan data sensitif lainnya melalui .env

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-secret-key-harus-diganti-di-env' 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'mysql+mysqlconnector://root:@localhost/projek_yolo8_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
